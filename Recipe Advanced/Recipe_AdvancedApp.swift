53877c5ba99c13218a3cb39b11c4c11b82fcea8b
Lukas Grunzke <lukasg@uchicago.edu>
2022-09-13 08:08:57 +0200

    add tabview

--- Recipe Advanced/Recipe_AdvancedApp.swift
+++ Recipe Advanced/Recipe_AdvancedApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Recipe_AdvancedApp: App {
     var body: some Scene {
         WindowGroup {
-            RecipeListView()
+            RecipeTabView()
         }
     }
 }
---
+++ Recipe Advanced/Views/RecipeTabView.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeTabView.swift
+//  Recipe Advanced
+//
+//  Created by Lukas on 9/13/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+            }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
