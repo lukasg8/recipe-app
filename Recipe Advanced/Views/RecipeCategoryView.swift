fd21416a5dbcbfc6eb1bbf3a3bbb9caa5a5ba37c
lukasg8 <102938476+lukasg8@users.noreply.github.com>
2022-10-25 16:21:58 -0500

    - implement design for vgrid for categories view

--- Recipe Advanced/Views/RecipeCategoryView.swift
+++ Recipe Advanced/Views/RecipeCategoryView.swift
@@ -19,23 +19,42 @@ struct RecipeCategoryView: View {
             
             Text("Categories")
                 .padding(.top, 40)
+                .padding(.leading)
                 .font(Font.custom("Avenir Heavy",size:28))
             
-            LazyVGrid (columns: [GridItem(.flexible(), spacing: 20, alignment: .top), GridItem(.flexible(), spacing: 20, alignment: .top)], alignment: .center, spacing: 20, content: {
+            GeometryReader { geo in
                 
-                ForEach(Array(model.categories), id: \.self) { category in
-                    
-                    Button(action: {
-                        // swithc tabs to list view
-                        selectedTab = Constants.listTab
-                        // set selected category
-                        model.selectedCategory = category
-                    }, label: {
-                        Text(category)
+                ScrollView (showsIndicators: false) {
+                    LazyVGrid (columns: [GridItem(.flexible(), spacing: 20, alignment: .top), GridItem(.flexible(), spacing: 20, alignment: .top)], alignment: .center, spacing: 20, content: {
+                        
+                        ForEach(Array(model.categories), id: \.self) { category in
+                            
+                            Button(action: {
+                                // swithc tabs to list view
+                                selectedTab = Constants.listTab
+                                // set selected category
+                                model.selectedCategory = category
+                            }, label: {
+                                VStack {
+                                    Image(category.lowercased())
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: (geo.size.width - 25)/2, height: (geo.size.width - 25)/2)
+                                        .cornerRadius(10)
+                                        .clipped()
+                                    Text(category)
+                                        .foregroundColor(.black)
+                                    
+                                }
+                            })
+                        }
                     })
-                    
                 }
-            })
+                .padding(.bottom,30)
+                
+            }
+            .padding(.horizontal)
+            
         }
     }
 }
